54d0813ed65ce9ba745650f2a1b6e32f09f4a71d
spiddy <dimitris.kapanidis@harbur.io>
2020-10-13 13:48:43 +0200

    Add GA

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,11 @@
 module.exports = {
   title: "Kubernetic",
   plugins: [
-    'vuepress-plugin-element-tabs'
+    'vuepress-plugin-element-tabs',
+    '@vuepress/google-analytics',
+    {
+      'ga': 'UA-11756963-5'
+    }
   ],
   themeConfig: {
     repo: 'harbur/kubernetic',
